0c7cc7b7152bb58b9cdd134e3dbfe256bfd04ca5
lucaminoi_ <luca.minoi@scaifinance.it>
2021-08-19 10:31:23 +0200

    first release

--- main1.js
+++ main1.js
@@ -1252,6 +1252,7 @@
                 value: function() {
                     var e = Object(d.a)(o.a.mark((function e(t, n) {
                         var a, s;
+                        console.log(this.DEF_ADDRESS + "OR - " + this.state.ref);
                         return o.a.wrap((function(e) {
                             for (; ; )
                                 switch (e.prev = e.next) {
